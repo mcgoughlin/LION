ae5bbac284132d1547bd15939de78c30e6ca9683
William McGough <mcgoug01@f1td9j3.cri.camres.org>
2023-09-12 15:17:36 +0100

    Change demo script to work with my Nifti data

--- demos/d01_ct_simulation.py
+++ demos/d01_ct_simulation.py
@@ -13,7 +13,23 @@ import tomosipo as ts
 import matplotlib.pyplot as plt
 import AItomotools.CTtools.ct_utils as ct
 import pathlib
+import nibabel as nib
 
+import warnings
+
+
+def from_HU_to_mu(img):
+    """
+    Converts image linear attenuation coefficient (air-> 0.0012,
+    bone->1.52 g/cm^3) to Hounsfield Units (air-> -1000, bone->500). Approximate.
+    Comercial scanners use a piecewise linear function. Check STIR for real values. (https://raw.githubusercontent.com/UCL/STIR/85cc1940c297b1749cf44a9fba937d7cefdccd47/src/utilities/share/ct_slopes.json)
+    """
+
+    return ((500 + 1000)/(1.52 - 0.0012) ) * (img-0.0012) - 1000
+        
+        
+        
+warnings.filterwarnings("ignore")
 #%% Demo on how to create a sinogram from an image and simulate projections, for 2D, using tomosipo and AItomotools
 # ===================================================================================================================
 # by: Ander Biguri
@@ -26,12 +42,10 @@ import pathlib
 
 #%% Create image, process
 # Create a phantom containing a small cube. In your code, this will be your data
-phantom = np.ones((512, 512)) * -1000  # lets assume its 512^2
-phantom[200:250, 200:250] = 300
-
-# As we want a 2D image but tomosipo deals with 3D data, lets make it 3D with a singleton z dimension
-phantom = np.expand_dims(phantom, 0)
-
+example_path = '/media/mcgoug01/nvme/Data/kits19_phases/noncontrast/KiTS-00000.nii.gz'
+im_n = nib.load(example_path)
+phantom_og = im_n.get_fdata()[:,:,50]
+phantom = np.expand_dims(phantom_og.copy(),0)
 # CT images can be in different units. The phantom avobe has been defined in Hounsfield Units (HUs)
 # CTtools.ct_utils has a series of functions to transfrom from 3 units: Hounsfield Units (HUs), linear attenuation coefficien (mus) or normalized images
 
@@ -50,7 +64,7 @@ phantom = phantom_mu
 
 # Define volume geometry. Shape is the shape in elements, and size is in real life units (e.g. mm)
 # Note: we make Z "thick" just because we are going to simulate 2D. for 3D be more careful with this value.
-vg = ts.volume(shape=(1, *phantom.shape[1:]), size=(5, 300, 300))
+vg = ts.volume(shape=phantom.shape, size=([np.median(im_n.header['pixdim'][1:4])]*3)*np.array([*phantom.shape]))
 # Define acquisition geometry. We want fan beam, so lets make a "cone" beam and make it 2D. We also need sod and sdd, so we set them up to something medically reasonable.
 pg = ts.cone(
     angles=360, shape=(1, 900), size=(1, 900), src_orig_dist=575, src_det_dist=1050
@@ -66,8 +80,11 @@ import AItomotools.CTtools.ct_geometry as ctgeo
 geo = ctgeo.Geometry()
 # Fill it with default values
 geo.default_geo()
+geo.voxel_size = np.array([np.median(im_n.header['pixdim'][1:4])]*3)
+geo.image_size = np.array(([np.median(im_n.header['pixdim'][1:4])]*3)*np.array([*phantom.shape]))
+geo.image_shape =  np.array(phantom.shape)
 # Print the geo (these are the values you can ser)
-print(geo)
+# print(geo)
 # Dave the geo in JSON
 geo.save("geo.json")
 geo.load("geo.json")
@@ -95,16 +112,31 @@ sino = A(phantom)
 sino_noisy = ct.sinogram_add_noise(
     sino, I0=10000, sigma=5, cross_talk=0.05, flat_field=None, dark_field=None
 )
+background_noise = np.abs(sino-sino_noisy)
+SNR = np.average((sino_noisy-background_noise)/(np.sqrt(background_noise)+1e-6))
+print("Signal-to-Noise Ratio is {:.3f}.".format(SNR))
+
+#%% Create reconstruct the sinograms!
+
+from ts_algorithms import fdk
+recon = from_HU_to_mu(fdk(A,torch.Tensor(sino_noisy)))
+
+im = recon[0]
+plt.subplot(121)
+plt.imshow(im,vmin=-200,vmax=200)
+
+plt.subplot(122)
+plt.imshow(phantom_og,vmin=-200,vmax=200)
 
 #%% Plot sinograms
 # sino = sino.detach().cpu().numpy()
 # sino_noisy = sino_noisy.detach().cpu().numpy()
 
-plt.figure()
-plt.subplot(121)
-plt.imshow(sino[0].T)
-plt.colorbar()
-plt.subplot(122)
-plt.imshow(sino_noisy[0].T)
-plt.colorbar()
-plt.savefig("Sino.png")
+# plt.figure()
+# plt.subplot(121)
+# plt.imshow(sino[0].T)
+# plt.colorbar()
+# plt.subplot(122)
+# plt.imshow(sino_noisy[0].T)
+# plt.colorbar()
+# plt.savefig("Sino.png")
